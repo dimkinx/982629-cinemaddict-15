6478c287d11b9088eeb64ea48677cef2383f48bd
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2021-09-05 18:03:26 +0300

    Add models to the app

---
+++ src/model/films-model.js
@@ -0,0 +1,16 @@
+import AbstractObserver from '../utils/abstract-observer.js';
+
+export default class FilmsModel extends AbstractObserver {
+  constructor() {
+    super();
+    this._films = [];
+  }
+
+  setFilms(films) {
+    this._films = films.slice();
+  }
+
+  getFilms() {
+    return this._films;
+  }
+}
